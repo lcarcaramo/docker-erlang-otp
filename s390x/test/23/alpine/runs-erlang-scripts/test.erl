d1776eecde896471072592ddfda206102687c1b0
lcarcaramo <72973973+lcarcaramo@users.noreply.github.com>
2020-11-02 14:34:40 -0500

    formatting

--- s390x/test/23/alpine/runs-erlang-scripts/test.erl
+++ s390x/test/23/alpine/runs-erlang-scripts/test.erl
@@ -1,8 +1,8 @@
 #!/usr/bin/env escript
 main(_) -> 
-	printGreen("Success! This script is running on the quay.io/ibmz/erlang:23.1.1 image.\n"),
+	printGreen("Success! This script is running on the quay.io/ibmz/erlang:23.1.1 image."),
 	printGreen("Terminating...").
 printGreen(STR) ->
 	ANSI_GREEN = "\e[32m",
 	ANSI_RESET = "\e[0m",
-	io:format("~s~s~s", [ANSI_GREEN, STR, ANSI_RESET]).
+	io:format("~s~s~s\n", [ANSI_GREEN, STR, ANSI_RESET]).
