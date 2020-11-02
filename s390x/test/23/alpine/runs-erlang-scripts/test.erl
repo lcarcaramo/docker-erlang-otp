f53d1fe0eb77aa216953f7868832b19c2021c01b
lcarcaramo <72973973+lcarcaramo@users.noreply.github.com>
2020-11-02 13:04:40 -0500

    Fix Syntax.

--- s390x/test/23/alpine/runs-erlang-scripts/test.erl
+++ s390x/test/23/alpine/runs-erlang-scripts/test.erl
@@ -1,4 +1,4 @@
 -module(test).
 -compile(export_all).
 
-io:format("Success! This script is running on the quay.io/ibmz/erlang:23.1.1 image.")
+io:format("Success! This script is running on the quay.io/ibmz/erlang:23.1.1 image.~n", []).
