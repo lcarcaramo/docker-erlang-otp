d83de6c8359036aaeb54cfb3a475c92118736cb5
lcarcaramo <72973973+lcarcaramo@users.noreply.github.com>
2020-11-02 13:29:46 -0500

    debug

--- s390x/test/23/alpine/runs-erlang-scripts/test.erl
+++ s390x/test/23/alpine/runs-erlang-scripts/test.erl
@@ -1,4 +1,4 @@
 -module(test).
 -export([test/0]).
 
-test() -> io:format("Success! This script is running on the quay.io/ibmz/erlang:23.1.1 image.~n", []);
+test() -> io:format("Success! This script is running on the quay.io/ibmz/erlang:23.1.1 image.~n");
